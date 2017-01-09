6e501213c38c7cf81981d494db97b4f9b80b60b7
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-01-08 19:17:20 -0500

    fixed HFlags usage with workaround

--- src/Main.hs
+++ src/Main.hs
@@ -9,12 +9,12 @@ import           Heos.Response
 import           HFlags
 import           System.Environment          (getArgs, getProgName)
 
---defineFlag "h:host" "192.168.0.19" "IP of any HEOS device in the network."
-flags_host = "192.168.0.19"
---defineFlag "n:name" "HEOS 1" "Name of the device to control."
-flags_name = "HEOS 1"
---defineFlag "p:playInput" "aux_in_1" "Input source to play."
-flags_playInput = "aux_in_1"
+defineFlag "h:host" "192.168.0.19" "IP of any HEOS device in the network."
+defineFlag "n:name" "HEOS 1" "Name of the device to control."
+defineFlag "play_input" "aux_in_1" "Input source to play."
+
+--workaround to known issue: https://github.com/nilcons/hflags/issues/14
+defineFlag "dummy" "dummy" "dummy flag"
 
 main :: IO ()
 main = do
@@ -24,10 +24,10 @@ main = do
   print $ show response
   --sources <- getMusicSources connection
   --print $ show sources
-  case flags_playInput of
+  case flags_play_input of
     "" -> putStrLn "no source specified"
     _ -> do
       inputChange <- case response of
-        Just p -> playInput p flags_playInput connection
+        Just p -> playInput p flags_play_input connection
         _      -> return $ responseError "" "player not found"
       return ()
